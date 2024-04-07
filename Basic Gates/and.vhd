5bbc477e3051b033818d4d06be447b6e0c49252e
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2024-04-07 17:04:15 +0530

    Create and.vhd

---
+++ Basic Gates/and.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity and_gate is
+port(
+  a,b: in std_logic;
+  q: out std_logic);
+end and_gate;
+
+architecture rtl of and_gate is
+begin
+  process(a,b) is
+  begin
+    q <= a and b;
+  end process;
+end rtl;
