47a738a386ed142a3076d00831ec0d4ab2ea2c67
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2024-04-07 23:53:14 +0530

    Update Behavioral.vhd

--- Flip-Flop/JK FlipFlop/Behavioral.vhd
+++ Flip-Flop/JK FlipFlop/Behavioral.vhd
@@ -2,20 +2,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity JKFF is
-port(J, K : in std_logic;
+port(J, K, CLK : in std_logic;
 	Q, Qnot : out std_logic);
 end JKFF;
 
 architecture Behavioural of JKFF is
 begin
-  process(J, K) is
+  process(J, K, CLK) is
   begin
+  	if rising_edge(CLK) then
   	if ((J xor K) = '1') then
       Q <= J;
       Qnot <= K;
     elsif (J='1' and K='1') then
     	Q <=  not Q;
         Qnot <= not Qnot;
+    end if;
     end if;
   end process;
 end Behavioural;
