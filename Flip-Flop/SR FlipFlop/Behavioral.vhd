9a29b648c85f351a8af82b2709cde305d40cf58f
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2024-04-07 18:23:29 +0530

    Update Behavioral.vhd

--- Flip-Flop/SR FlipFlop/Behavioral.vhd
+++ Flip-Flop/SR FlipFlop/Behavioral.vhd
@@ -2,17 +2,19 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity SRFF is
-port(S, R : in std_logic;
+port(S, R, CLK : in std_logic;
 	Q, Qnot : out std_logic);
 end SRFF;
 
 architecture Behavioural of SRFF is
 begin
-  process(S, R) is
+  process(S, R, CLK) is
   begin
-  	if ((S xor R) = '1') then
-      Q <= S;
-      Qnot <= R;
-    end if;
+  	if rising_edge(CLK) then
+    	if ((S xor R) = '1') then
+      		Q <= S;
+      		Qnot <= R;
+    	end if;
+	end if;
   end process;
 end Behavioural;
