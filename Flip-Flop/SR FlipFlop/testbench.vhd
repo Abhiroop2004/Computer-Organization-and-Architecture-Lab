f833658782702103085930914b3cd661d09ca6f2
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2024-04-07 23:47:44 +0530

    Update testbench.vhd

--- Flip-Flop/SR FlipFlop/testbench.vhd
+++ Flip-Flop/SR FlipFlop/testbench.vhd
@@ -1,31 +1,37 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity testbench is
--- empty
-end testbench; 
+entity SRFF_tb is
+end entity; 
 
-architecture tb of testbench is
-
+architecture tb of SRFF_tb is
 component SRFF
-port(S, R : in std_logic;
+port(S, R, CLK : in std_logic;
 	Q, Qnot : out std_logic);
 end component;
 
-signal S, R, Q, Qnot : std_logic;
+signal S, R, CLK, Q, Qnot : std_logic;
 
 begin
-DUT: SRFF port map (S, R, Q, Qnot);
+DUT: SRFF port map (S, R, CLK, Q, Qnot);
     process
     begin
-    	  S <= '1';   R <='0';   wait for 10 ns;
-        assert (Q='1' and Qnot='0') report "Fail 1/0" severity error;
-        S <= '0';   R <='1';   wait for 10 ns;
-        assert (Q='0' and Qnot='1') report "Fail 0/1" severity error;
-        S <= '0';   R <='0';   wait for 10 ns;
+		S <= '0';   R <='0'; CLK <='0';  wait for 10 ns;
+        assert (Q='0' and Qnot='1') report "Fail 0/0" severity error;
+        S <= '0';   R <='0'; CLK <='1';  wait for 10 ns;
         assert (Q='0' and Qnot='1') report "Fail 0/0" severity error;
         
-        assert false report "Test done." severity note;
+    	S <= '1';   R <='0'; CLK <='0';  wait for 10 ns;
+        assert (Q='0' and Qnot='1') report "Fail 1/0" severity error;
+        S <= '1';   R <='0'; CLK <='1';  wait for 10 ns;
+        assert (Q='1' and Qnot='0') report "Fail 1/0" severity error;
+        
+        S <= '0';   R <='1'; CLK <='0';  wait for 10 ns;
+        assert (Q='1' and Qnot='0') report "Fail 0/1" severity error;
+        S <= '0';   R <='1'; CLK <='1';  wait for 10 ns;
+        assert (Q='0' and Qnot='1') report "Fail 0/1" severity error;
+                
+        assert false report "Test done." severity note;                
     wait;
     end process;
 end; 
