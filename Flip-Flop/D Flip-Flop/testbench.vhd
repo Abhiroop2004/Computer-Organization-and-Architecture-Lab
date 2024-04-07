2d8d03496cfdcdc42067462db44fda1716d0d3a8
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2024-04-08 05:03:50 +0530

    Update testbench.vhd

--- Flip-Flop/D Flip-Flop/testbench.vhd
+++ Flip-Flop/D Flip-Flop/testbench.vhd
@@ -6,58 +6,30 @@ end entity;
 
 architecture tb of DFF_tb is
 component D_FLIPFLOP_SOURCE is
-Port ( D, CLK, RST : in STD_LOGIC;
+Port ( D, CLK : in STD_LOGIC;
 Q, Qb : out STD_LOGIC);
 end component ;
 
-signal D, CLK, RST, Q, Qb : STD_LOGIC;
+signal D, CLK, Q, Qb : STD_LOGIC;
 
 begin
-dut: D_FLIPFLOP_SOURCE port map(
-D => D,
-CLK => CLK,
-RST => RST,
-Q => Q,
-Qb => Qb);
+dut: D_FLIPFLOP_SOURCE port map( D, CLK, Q, Qb);
 
-
---CLK <= not CLK after 1 ns;
-  clk_process: process
-  begin
-  	for i in 1 to 4 loop
-    	CLK <= '1';
-        wait for 1 ns;
-        CLK <= '0';
-        wait for 1 ns;
-    end loop;
-  wait;
-  end process;
-  
-t:process
+process
 begin
-	RST <= '0';
+	CLK <= '0';
 	D <= '0';
-wait for 1 ns;
-	RST <= '1';
-	D <= '0';
-wait for 1 ns;
-	RST <= '0';
-    D <= '1';
-wait for 1 ns;
-	RST <= '1';
-    D <= '1';
-wait for 1 ns;
-    RST <= '0';
-	D <= '0';
-wait for 1 ns;
-	RST <= '1';
-	D <= '0';
-wait for 1 ns;
-	RST <= '0';
-    D <= '1';
-wait for 1 ns;
-	RST <= '1';
-    D <= '1';
+wait for 10 ns; assert (Q='0' and Qb='0') report "Fail 1/0" severity error;
+	CLK <= '1';
+    D <= '0';
+wait for 10 ns; assert (Q='0' and Qb='1') report "Fail 1/0" severity error;
+	CLK <= '0';
+    D <= '1'; 
+wait for 10 ns; assert (Q='0' and Qb='1') report "Fail 1/0" severity error;
+    CLK <= '1';
+	D <= '1'; 
+wait for 10 ns; assert (Q='1' and Qb='0') report "Fail 1/0" severity error;
+
 wait;
 end process;
 end tb;
