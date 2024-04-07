b7375aa60ddae1175a67354b8c41c358e784e6fd
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2024-04-07 17:02:55 +0530

    Create not.vhd

---
+++ Basic Gates/not.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity not_gate is
+port(
+  a: in std_logic;
+  q: out std_logic);
+end not_gate;
+
+architecture rtl of not_gate is
+begin
+  process(a) is
+  begin
+    q <= not a;
+  end process;
+end rtl;
