fd0ab7677959b803cb24bf8f5cf9a86e764fcb7a
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2024-04-07 23:52:19 +0530

    Update testbench.vhd

--- Flip-Flop/JK FlipFlop/testbench.vhd
+++ Flip-Flop/JK FlipFlop/testbench.vhd
@@ -8,23 +8,34 @@ end testbench;
 architecture tb of testbench is
 
 component JKFF
-port(J, K : in std_logic;
+port(J, K, CLK : in std_logic;
 	Q, Qnot : out std_logic);
 end component;
 
-signal J, K, Q, Qnot : std_logic;
+signal J, K, CLK, Q, Qnot : std_logic;
 
 begin
-DUT: JKFF port map (J, K, Q, Qnot);
+DUT: JKFF port map (J, K, CLK, Q, Qnot);
     process
     begin
-        J <= '1';   K <='0';   wait for 10 ns;
+        J <= '1';   K <='0'; CLK <='0';  wait for 10 ns;
         assert (Q='1' and Qnot='0') report "Fail 1/0" severity error;
-        J <= '0';   K <='1';   wait for 10 ns;
+        J <= '1';   K <='0'; CLK <='1';  wait for 10 ns;
+        assert (Q='1' and Qnot='0') report "Fail 1/0" severity error;
+        
+        J <= '0';   K <='1'; CLK <='0';  wait for 10 ns;
+        assert (Q='1' and Qnot='0') report "Fail 0/1" severity error;
+        J <= '0';   K <='1'; CLK <='1';  wait for 10 ns;
         assert (Q='0' and Qnot='1') report "Fail 0/1" severity error;
-        J <= '1';   K <='1';   wait for 10 ns;
+        
+        J <= '1';   K <='1'; CLK <='0';  wait for 10 ns;
+        assert (Q='0' and Qnot='1') report "Fail 1/1" severity error;
+        J <= '1';   K <='1'; CLK <='1';  wait for 10 ns;
         assert (Q='1' and Qnot='0') report "Fail 1/1" severity error;
-        J <= '0';   K <='0';   wait for 10 ns;
+        
+        J <= '0';   K <='0'; CLK <='0';  wait for 10 ns;
+        assert (Q='1' and Qnot='0') report "Fail 0/0" severity error;
+        J <= '0';   K <='0'; CLK <='1';  wait for 10 ns;
         assert (Q='1' and Qnot='0') report "Fail 0/0" severity error;
         
         assert false report "Test done." severity note;
