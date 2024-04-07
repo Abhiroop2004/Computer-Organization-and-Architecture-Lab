eeb8bdc0184395484cd54a547ca5313bd79d044b
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2024-04-08 04:58:07 +0530

    Update behavorial.vhd

--- Flip-Flop/D Flip-Flop/behavorial.vhd
+++ Flip-Flop/D Flip-Flop/behavorial.vhd
@@ -2,20 +2,22 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity D_FLIPFLOP_SOURCE is
-Port ( D, CLK, RST : in  STD_LOGIC;
+Port ( D, CLK : in  STD_LOGIC;
 Q, Qb : out  STD_LOGIC);
 end D_FLIPFLOP_SOURCE;
 
 architecture Behavioral of D_FLIPFLOP_SOURCE is
 begin
-process (D, CLK, RST)
-begin
-if(RST = '1') then
-	Q <= '0';
-    Qb <= '1';         
-elsif rising_edge(CLK) then 
-    Q <= D;
-	Qb <= (not D);   
+process (D, CLK)
+begin     
+if (CLK='1') then 
+    if (D='0') then
+        Q <= '0';
+        Qb <= '1';
+    else
+        Q <= '1';
+        Qb <= '0';
+     end if;   
 end if;
 end process;
 end Behavioral;
